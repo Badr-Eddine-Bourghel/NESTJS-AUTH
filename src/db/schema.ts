f2b469ea8f56f5a2f91a53dd9ce89b8939b2a37b
Badr-Eddine-Bourghel <BOURGHEL.BADREDDINE@ensam-casa.ma>
2026-07-24 21:53:54 +0100

    Auth Service

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -44,6 +44,6 @@ export const tasks = pgTable('tasks', {
 });
 
 export type User = typeof users.$inferSelect;
-export type NewUser = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
 export type Task = typeof tasks.$inferSelect;
-export type NewTask = typeof tasks.$inferSelect;
+export type NewTask = typeof tasks.$inferInsert;
