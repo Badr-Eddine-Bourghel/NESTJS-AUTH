0839a9a3b4cb0b78170bc1acfb0346eced70a4b5
Badr-Eddine-Bourghel <BOURGHEL.BADREDDINE@ensam-casa.ma>
2026-07-25 20:38:35 +0100

    Public Decorator

---
+++ src/auth/auth.controler.ts
@@ -0,0 +1,23 @@
+import {
+  Constroller,
+  Post,
+  Get,
+  Body,
+  Query,
+  Req,
+  Res,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiCookieAuth,
+} from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { Public } from '../common/decorators/public.decorator';
+import type { User } from 'src/db/schema';
