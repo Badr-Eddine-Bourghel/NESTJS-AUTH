cd2f41150b0792d41a49cadd6d5417a437eb3446
Badr-Eddine-Bourghel <BOURGHEL.BADREDDINE@ensam-casa.ma>
2026-07-26 21:25:03 +0100

    Fixing errors

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,6 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
-const sql = neon(process.env.DATABASE_RUL!);
+const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
